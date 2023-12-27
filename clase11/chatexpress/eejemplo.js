3c9c303db4baa1229bf79db6b8256c3193fccea1
Luis Menendez <lmenendez.1@gmail.com>
2023-12-27 14:39:05 -0600

    registro de comandos clase 12

---
+++ clase11/chatexpress/eejemplo.js
@@ -0,0 +1,27 @@
+let estudiantes = [
+  {
+    name: "Alvaro",
+    lastname: "Perez",
+    email: "alvaro@gmail.com",
+    age: 25,
+  },
+  {
+    name: "Juan",
+    lastname: "Gomez",
+    email: "juan@gmail.com",
+    age: 30,
+  },
+
+  {
+    name: "Maria",
+    lastname: "Gomez",
+    email: "maria@gmail.com",
+    age: 28,
+  },
+  {
+    name: "Pedro",
+    lastname: "Garcia",
+    email: "pedro@gmail.com",
+    age: 35,
+  },
+];
